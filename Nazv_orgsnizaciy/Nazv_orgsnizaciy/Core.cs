c664677187d9b46dcc4a5b5d256974f08c453f99
KirkNovos <kirknovos@mail.ru>
2021-01-14 11:11:53 +0300

    исправлена база данных и добавлена таблица с информацией с базы данных

---
+++ Nazv_orgsnizaciy/Nazv_orgsnizaciy/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nazv_orgsnizaciy
+{
+    class Core
+    {
+        public static vtoporovaEntities DB = new vtoporovaEntities();
+   
+}
+    
+}
